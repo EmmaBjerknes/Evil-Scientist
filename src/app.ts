1ec0954fdd8b7b9e6376e54c2969f80e683ce307
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-01-10 13:51:43 +0100

    Added a array of of Obj, got the obj.name to show in html

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,45 @@
-let o: {
-    x: number,
-    y: string,
-} = {
-    x: 12,
-    y: "hej",
+type Scientist = {
+    name: string,
+    age: number,
+    minions: number,
+    description: string
 }
 
-console.log(o);
+const arrScientists: Scientist[] =[
+    {
+        name: "Greger",
+        age: 34,
+        minions: 2,
+        description: "Evil overlord with two sidekicks."
+
+    },
+    {
+        name: "Hans",
+        age: 215,
+        minions: 23,
+        description: "Alien friendly"
+
+    },
+    {
+        name: "Berit",
+        age: 215,
+        minions: 23,
+        description: "Alien friendly"
+
+    },
+];
+
+
+let myDiv = document.getElementById("scientistListDiv") as HTMLDivElement;
+
+function getScientistName(){
+    for(const scientist of arrScientists){
+        const scientistList = document.createElement("div");
+        scientistList.innerHTML = scientist.name;
+        scientistList.style.border ="2px solid black"
+        myDiv.append(scientistList);
+        console.log(scientist);
+    } 
+}
+
+getScientistName();
